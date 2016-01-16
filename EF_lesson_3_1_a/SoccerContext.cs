368d7088e7c26588920c0b2c1de8bdb3fb838f91
Alex <alexis1784@gmail.com>
2016-01-16 13:22:26 +0500

    have added code of example from http://metanit.com/sharp/entityframework/3.1.php

--- EF_lesson_3_1_a/SoccerContext.cs
+++ EF_lesson_3_1_a/SoccerContext.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace EF_lesson_3_1_a
 {
-    class SoccerContext
+    class SoccerContext : DbContext
     {
+        public SoccerContext()
+            : base("DefaultConnection")
+        { }
+
+        public DbSet<Player> Players { get; set; }
     }
 }
