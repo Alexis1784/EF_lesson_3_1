72d02af36df845bdebc1d93f9657ea04141e8fb4
Alex <alexis1784@gmail.com>
2016-01-19 11:27:59 +0500

    after adding Team consturctor

--- EF_lesson_3_1_a/Team.cs
+++ EF_lesson_3_1_a/Team.cs
@@ -8,6 +8,8 @@ namespace EF_lesson_3_1_a
 {
     class Team
     {
+        public Team (string Name)
+        { this.Name = Name; }
         public int Id { get; set; }
         public string Name { get; set; } // название команды
         public string Coach { get; set; } // тренер
